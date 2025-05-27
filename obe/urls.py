613aed4d2e2931c017824e578cc3b31a398a63f5
Durjoy <57084424+dee4git@users.noreply.github.com>
2025-05-27 17:48:46 +0600

    ok

--- obe/urls.py
+++ obe/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
 ]
--- obe/views.py
+++ obe/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request,'home.html')
+    context = {}
+
+    if request.user.is_authenticated and request.user.is_superuser:
+        context['username'] = request.user.username
+        context['is_superuser'] = True
+    else:
+        context['username'] = request.user.username if request.user.is_authenticated else 'Guest'
+        context['is_superuser'] = False
+
+    return render(request,'home.html', context)
